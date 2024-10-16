847d69ec26ed4c51f27e8d58d31dbd207f08fe3a
hojat-gazestani <hojatgazestani@gmail.com>
2024-10-16 22:57:51 +0000

    feature logging added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+import logging
 from typing import Final
 from telegram.ext import (
     Application,
@@ -19,6 +21,13 @@ from handlers.commands import (
 from handlers.utils import handle_message
 
 
+# Initialize logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 load_dotenv()
 TOKEN: Final = os.getenv("DENALIE_MOVIE_BOT_TOKEN")
@@ -32,7 +41,7 @@ RULES_OPTION = "📜 قوانین گروه"
 
 
 def main() -> None:
-    print("Starting bot...")
+    logger.info("Starting bot...")
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start_command))
@@ -67,7 +76,7 @@ def main() -> None:
 
     #app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
-    print("Polling ...")
+    logger.info("Polling started...")
     app.run_polling(poll_interval=0.5)
 
 
